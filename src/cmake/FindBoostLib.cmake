f70b3f167a91c61db9dc14bd730a74501d47ad37
Nick Rizzo <nickmike55@gmail.com>
2020-10-06 00:54:06 -0400

    [rm] Unneeded compile prints

--- src/cmake/FindBoostLib.cmake
+++ src/cmake/FindBoostLib.cmake
@@ -1,6 +1,5 @@
 # Find Boost libraries -- https://cmake.org/cmake/help/v3.14/module/FindBoost.html
 
-message(STATUS "Boost_INCLUDE_DIR: ${Boost_INCLUDE_DIRS}")
 # set(Boost_ADDITIONAL_VERSIONS "1.48" "1.48.0")
 set(Boost_USE_STATIC_LIBS        ON)  # only find static libs
 set(Boost_USE_DEBUG_LIBS         OFF) # ignore debug libs and
@@ -9,4 +8,3 @@ set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_RUNTIME    OFF)
 find_package(Boost COMPONENTS system thread regex filesystem REQUIRED)
 include_directories(${Boost_INCLUDE_DIRS})
-message(STATUS "Finished boost")
