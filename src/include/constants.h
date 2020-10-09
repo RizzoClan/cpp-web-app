53bb89ad9c48acf134858ebe3b275c3b7a89e033
Nick Rizzo <nickmike55@gmail.com>
2020-10-08 22:21:19 -0400

    [feat] Added a constants file to prevent magic numbers

---
+++ src/include/constants.h
@@ -0,0 +1,11 @@
+// @File: This file is meant to store constants that would otherwise be magic values
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+const std::string CLI_PARSER_NAME = "Basic Crow Web App";
+const std::string URL_BASE = "http://localhost:";
+
+#endif
